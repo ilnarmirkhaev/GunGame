6bbcd24c4ed2ce8251a7634040bdb295196b1c5b
ilnarmirkhaev <56744129+ilnarmirkhaev@users.noreply.github.com>
2022-06-06 01:03:29 +0300

    Lots of stuff

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+namespace UI
 {
-    public void PlayGame()
+    public class MainMenu : MonoBehaviour
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
+        public void PlayGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
 
-    public void QuitGame()
-    {
-        Debug.Log("QUIT");
-        Application.Quit();
+        public void QuitGame()
+        {
+            Debug.Log("QUIT");
+            Application.Quit();
+        }
     }
 }
